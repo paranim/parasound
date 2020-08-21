2b095bd72e6e5f0f24603335e3af8db306329a64
oakes <zach@sekao.net>
2020-08-21 16:44:06 -0400

    Add test for in-memory writing/playing

--- tests/test1.nim
+++ tests/test1.nim
@@ -21,7 +21,7 @@ test "can write wav file":
   format.channels = 1
   format.sampleRate = sampleRate
   format.bitsPerSample = 16
-  doAssert drwav_init_file_write(wav.addr, "middle_c.wav", addr(format), nil)
+  doAssert drwav_init_file_write(wav.addr, "middle_c.wav", format.addr, nil)
   doAssert numSamples == drwav_write_pcm_frames(wav.addr, numSamples, data[0].addr)
   discard drwav_uninit(wav.addr)
   doAssert existsFile("middle_c.wav")
@@ -62,3 +62,58 @@ test "can play wav file":
 test "can play mp3 file":
   playFile("tests/xylophone-sweep.mp3", 2000)
 
+proc playMemory(data: pointer, dataSize: csize, sleepMsecs: int) =
+  var
+    decoder = newSeq[uint8](ma_decoder_size())
+    decoderAddr = cast[ptr ma_decoder](decoder[0].addr)
+    deviceConfig = newSeq[uint8](ma_device_config_size())
+    deviceConfigAddr = cast[ptr ma_device_config](deviceConfig[0].addr)
+    device = newSeq[uint8](ma_device_size())
+    deviceAddr = cast[ptr ma_device](device[0].addr)
+  doAssert MA_SUCCESS == ma_decoder_init_memory_wav(data, dataSize, nil, decoderAddr)
+
+  proc data_callback(pDevice: ptr ma_device; pOutput: pointer; pInput: pointer; frameCount: ma_uint32) {.cdecl.} =
+    let decoderAddr = ma_device_get_decoder(pDevice)
+    discard ma_decoder_read_pcm_frames(decoderAddr, pOutput, frameCount)
+
+  ma_device_config_init_with_decoder(deviceConfigAddr, ma_device_type_playback, decoderAddr, data_callback)
+  if ma_device_init(nil, deviceConfigAddr, deviceAddr) != MA_SUCCESS:
+    discard ma_decoder_uninit(decoderAddr)
+    quit("Failed to open playback device.")
+
+  if ma_device_start(deviceAddr) != MA_SUCCESS:
+    ma_device_uninit(deviceAddr)
+    discard ma_decoder_uninit(decoderAddr)
+    quit("Failed to start playback device.")
+
+  sleep(sleepMsecs)
+  discard ma_device_stop(deviceAddr)
+  ma_device_uninit(deviceAddr)
+  discard ma_decoder_uninit(decoderAddr)
+
+test "can write wav to memory and play it":
+  const
+    middleC = staticRead("middle_c.json")
+    sampleRate = 44100
+    numSamples = 1 * sampleRate
+  let arr = parseJson(middleC)
+  var
+    data = newSeq[cshort]()
+    wav: drwav
+    format: drwav_data_format
+  for n in arr:
+    data.add(n.getInt.cshort)
+  format.container = drwav_container_riff
+  format.format = DR_WAVE_FORMAT_PCM
+  format.channels = 1
+  format.sampleRate = sampleRate
+  format.bitsPerSample = 16
+  var
+    output: pointer
+    outputSize: csize
+  doAssert drwav_init_memory_write_sequential(wav.addr, output.addr, outputSize.addr, format.addr, numSamples, nil)
+  doAssert numSamples == drwav_write_pcm_frames(wav.addr, numSamples, data[0].addr)
+  doAssert outputSize > 0
+  playMemory(output, outputSize, 1000)
+  discard drwav_uninit(wav.addr)
+  drwav_free(output, nil)
